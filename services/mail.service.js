c5e6aab49cc7a736e229d7c8d726104ca4ef9aa2
Mihai Smolenschi <mihaismolenski@gmail.com>
2023-07-04 22:17:09 +0300

    upadte mail service

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,30 +1,33 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 module.exports = function (app) {
-    app.post('/sendEmail', async (req, res) => {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'mihaismolenski@gmail.com',
-                pass: 'fsdovayumdsmxdxw',
-            },
-        });
-
-        const mailOptions = {
-            from: req.body.email,
-            to: 'mihaismolenski@gmail.com',
-            subject: `Message from PrushaMedia.ro client [${req.body.email}]`,
-            text: req.body.message
-        };
-
-        await transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-        res.json({success: true});
+  app.post("/sendEmail", async (req, res) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "mihaismolenski@gmail.com",
+        pass: "fsdovayumdsmxdxw",
+      },
     });
+
+    const mailOptions = {
+      from: "PrushaMedia.ro",
+      to: "mihaismolenski@gmail.com",
+      subject: `[Message from PrushaMedia.ro client]`,
+      html: `<b>From:</b> ${req.body.name}<br />
+        <b>Phone:</b> ${req.body.phone}<br />
+        <b>Email:</b> ${req.body.email}<br />
+        <b>Message:</b> ${req.body.message}`,
+    };
+
+    await transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+    res.json({ success: true });
+  });
 };
